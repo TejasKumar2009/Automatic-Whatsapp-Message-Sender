5ec98f2127f58b2ef5fd894a5a673cb3b8d687ad
TejasKumar2009 <79413665+TejasKumar2009@users.noreply.github.com>
2021-02-25 21:09:13 +0530

    WhatsApp message Sender

---
+++ main.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome("C:\Python\Lib\site-packages\selenium\webdriver\chrome\chromedriver")
+
+driver.get("https://web.whatsapp.com/")
+
+name = input("Enter The Group or User name: ")
+message = input("Enter your message: ")
+count = int(input("Enter the times of message: "))
+
+scode = input("Enter anything after scan code:")
+
+user = driver.find_element_by_xpath('//span[@title="{}"]'.format(name))
+user.click()
+
+msg_box = driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
+
+for i in range(count):
+
+   msg_box.send_keys(message)
+   driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button").click()
+
+print("Message Sent Sucessfully!!")
